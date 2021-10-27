397cafef9d8bd7cd748312f356c8baa3ea9d5078
FERDdeveloper <86313430+FERDdeveloper@users.noreply.github.com>
2021-10-27 11:12:50 +0200

    GENOCIDE

---
+++ me/ferderplays/FHook/FHook.java
@@ -0,0 +1,21 @@
+public class FHook {
+    Static objects() {
+        window1 = new Window();
+        if(mc.runned()) {
+            do {
+                this.openWindow(window1);
+            }
+        }
+    }
+    void windowOpened() {
+        window1p = new ArrayList<String>;
+        window1p.add(lol1);
+        window1p.add(lol2);
+        void crackclientloaded(process = 5%) {
+            String lol1 = System.out.printIn("[Hulkw]:" + "loading clientz... 5/100").in(window1);
+        }
+        void crackclientloaded(process = 10%) {
+            String lol2 = System.out.printIn("[Hulkw]:" + "loading clientz... 10/100").in(window1);
+        }
+    }
+}
